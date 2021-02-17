20992b9bc24a872b2cf6250efe657341a68f5841
pat <Phatscub2589@gmail.com>
2021-02-17 12:49:56 +0700

    first commit

---
+++ react/test/src/items.js
@@ -0,0 +1,24 @@
+const items = [{
+        title: "CAT_1",
+        thumbnaiUrl: "/1.jpg"
+
+
+    },
+    {
+        title: "CAT_2",
+        thumbnaiUrl: "/2.jpg"
+    },
+    {
+        title: "CAT_3",
+        thumbnaiUrl: "/3.jpg"
+
+
+    },
+    {
+        title: "CAT_4",
+        thumbnaiUrl: "/4.jpg"
+
+
+    }
+];
+export default items;
